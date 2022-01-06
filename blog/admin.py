cc2eb8b6a7d411d05f72a84bf781657d4809a909
priscilla254 <priscillawambui8@gmail.com>
2022-01-06 21:46:46 +0300

    my portfolio app with blog

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from blog.models import *
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category,CategoryAdmin)
+
+
